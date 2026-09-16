06146aba2acc781d436a22c3aa7cb3bff3c104e5
Tee-David <68272665+Tee-David@users.noreply.github.com>
2026-09-16 16:41:02 +0530

    feat: expose public API

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { createNairaGate } from "./client.js";
+export type { NairaGateOptions } from "./client.js";
+export { NairaGateError } from "./errors.js";
+export type { NairaGateErrorCode } from "./errors.js";
+export { PaystackProvider } from "./providers/paystack.js";
+export type { PaystackProviderOptions } from "./providers/paystack.js";
+export type { Bank, BankProvider, ResolveAccountInput, ResolvedAccount } from "./types.js";
